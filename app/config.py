1e76ba76a14b3742ceae86293488590d7594e046
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-03-08 20:04:34 +0900

    settings

---
+++ app/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    kafka_brokers: str = "localhost:9092"
+    redis_url: str = "redis://localhost:6379/0"
+    group_id: str = "event-stream"
+
+
+settings = Settings()
