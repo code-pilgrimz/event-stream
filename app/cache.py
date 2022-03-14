cc10b69adaf6e8e695ac18e368b1053d60f418a8
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-03-14 13:11:18 +0900

    redis cache

---
+++ app/cache.py
@@ -0,0 +1,12 @@
+import redis
+from app.config import settings
+
+_client = redis.from_url(settings.redis_url)
+
+
+def get(key: str):
+    return _client.get(key)
+
+
+def set(key: str, value: str, ttl: int = 3600):
+    _client.setex(key, ttl, value)
