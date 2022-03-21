a89e8f61e081f8dadca97e9716b45437e882989f
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-03-21 19:42:29 +0900

    kafka producer

---
+++ app/producer.py
@@ -0,0 +1,9 @@
+from confluent_kafka import Producer
+from app.config import settings
+
+_producer = Producer({"bootstrap.servers": settings.kafka_brokers})
+
+
+def publish(topic: str, key: str, value: bytes):
+    _producer.produce(topic, key=key, value=value)
+    _producer.poll(0)
