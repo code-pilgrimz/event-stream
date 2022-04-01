a80825fbe634301f80d7a222d3a49f18f050189e
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-04-01 09:10:35 +0900

    wip tests

--- app/consumer.py
+++ app/consumer.py
@@ -6,3 +6,4 @@ def make_consumer(topics):
     c = Consumer({"bootstrap.servers": settings.kafka_brokers, "group.id": settings.group_id, "auto.offset.reset": "earliest"})
     c.subscribe(topics)
     return c
+# check perf here
