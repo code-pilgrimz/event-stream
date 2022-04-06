facada17cbaf69048b39b353a28ac4bf029466c5
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-04-06 09:05:32 +0900

    user handler

---
+++ app/handlers/user.py
@@ -0,0 +1,10 @@
+import orjson
+from app.events.user import UserEvent
+from app import cache
+
+TOPIC = "user.events"
+
+
+def handle(payload: bytes) -> None:
+    event = UserEvent(**orjson.loads(payload))
+    cache.set(f"user:{event.id}", payload.decode())
