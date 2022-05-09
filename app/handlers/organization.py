0a12c42e6ea9bbec518f427a1a9401869b698296
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-05-09 11:13:58 +0900

    organization handler

---
+++ app/handlers/organization.py
@@ -0,0 +1,10 @@
+import orjson
+from app.events.organization import OrganizationEvent
+from app import cache
+
+TOPIC = "organization.events"
+
+
+def handle(payload: bytes) -> None:
+    event = OrganizationEvent(**orjson.loads(payload))
+    cache.set(f"organization:{event.id}", payload.decode())
