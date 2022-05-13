bda1058fa6c2acc88d64ee1639be94fcc8915ac4
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-05-13 16:49:16 +0900

    project handler

---
+++ app/handlers/project.py
@@ -0,0 +1,10 @@
+import orjson
+from app.events.project import ProjectEvent
+from app import cache
+
+TOPIC = "project.events"
+
+
+def handle(payload: bytes) -> None:
+    event = ProjectEvent(**orjson.loads(payload))
+    cache.set(f"project:{event.id}", payload.decode())
