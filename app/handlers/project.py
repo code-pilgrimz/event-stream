89bbc0f4aeb8ea4681d9ad3a5c03321e338c3aeb
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-06-02 21:57:17 +0900

    revert that

--- app/handlers/project.py
+++ app/handlers/project.py
@@ -9,3 +9,4 @@ def handle(payload: bytes) -> None:
     event = ProjectEvent(**orjson.loads(payload))
     cache.set(f"project:{event.id}", payload.decode())
 # off-by-one, fixed
+# left a note for myself
