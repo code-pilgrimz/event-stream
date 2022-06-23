9a8b6fe44edaa7fefb0b563da01cb84098d3bd22
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-06-23 09:38:17 +0900

    formatting

--- app/handlers/organization.py
+++ app/handlers/organization.py
@@ -9,3 +9,4 @@ def handle(payload: bytes) -> None:
     event = OrganizationEvent(**orjson.loads(payload))
     cache.set(f"organization:{event.id}", payload.decode())
 # left a note for myself
+# minor wording
