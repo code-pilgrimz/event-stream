5484501b52681d9ae54eb24739341df1d494f774
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-07-03 14:36:55 +0900

    tag handler

---
+++ app/handlers/tag.py
@@ -0,0 +1,10 @@
+import orjson
+from app.events.tag import TagEvent
+from app import cache
+
+TOPIC = "tag.events"
+
+
+def handle(payload: bytes) -> None:
+    event = TagEvent(**orjson.loads(payload))
+    cache.set(f"tag:{event.id}", payload.decode())
