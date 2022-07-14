48cdf283f08bd550f9de4eabbac9c2237059960c
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-07-14 17:02:59 +0900

    invoice handler

---
+++ app/handlers/invoice.py
@@ -0,0 +1,10 @@
+import orjson
+from app.events.invoice import InvoiceEvent
+from app import cache
+
+TOPIC = "invoice.events"
+
+
+def handle(payload: bytes) -> None:
+    event = InvoiceEvent(**orjson.loads(payload))
+    cache.set(f"invoice:{event.id}", payload.decode())
