4da96c29ee84d1c22ff4771733212d4386f38bb7
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-08-03 18:26:39 +0900

    payment handler

---
+++ app/handlers/payment.py
@@ -0,0 +1,10 @@
+import orjson
+from app.events.payment import PaymentEvent
+from app import cache
+
+TOPIC = "payment.events"
+
+
+def handle(payload: bytes) -> None:
+    event = PaymentEvent(**orjson.loads(payload))
+    cache.set(f"payment:{event.id}", payload.decode())
