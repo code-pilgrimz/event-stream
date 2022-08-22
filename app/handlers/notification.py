3a37055053e334f40c506cda61bb8383499afcfb
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-08-22 10:28:48 +0900

    notification handler

---
+++ app/handlers/notification.py
@@ -0,0 +1,10 @@
+import orjson
+from app.events.notification import NotificationEvent
+from app import cache
+
+TOPIC = "notification.events"
+
+
+def handle(payload: bytes) -> None:
+    event = NotificationEvent(**orjson.loads(payload))
+    cache.set(f"notification:{event.id}", payload.decode())
