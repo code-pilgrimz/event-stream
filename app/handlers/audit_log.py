213d1a6843900fa771e4831aa03ad3d0a529cc03
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-09-21 10:27:47 +0900

    audit_log handler

---
+++ app/handlers/audit_log.py
@@ -0,0 +1,10 @@
+import orjson
+from app.events.audit_log import AuditLogEvent
+from app import cache
+
+TOPIC = "audit_log.events"
+
+
+def handle(payload: bytes) -> None:
+    event = AuditLogEvent(**orjson.loads(payload))
+    cache.set(f"audit_log:{event.id}", payload.decode())
