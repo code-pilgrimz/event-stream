1e5131f09ebde2cc71437151f5d50678b6521027
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-10-11 21:30:21 +0900

    polish

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -4,3 +4,4 @@ from app.events.user import UserEvent
 def test_user_event():
     e = UserEvent(id=1, email="a@b.c", full_name="a", is_active=True, hashed_password="x")
     assert e.id == 1
+# left a note for myself
